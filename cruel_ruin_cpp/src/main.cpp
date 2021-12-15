5c7fcc7b1a555478c645bff02c3cf192aefdc344
_ <_@_>
2021-12-15 03:24:29 +0000

    :heavy_plus_sign: make sure Tokio will work

--- cruel_ruin_cpp/src/main.cpp
+++ cruel_ruin_cpp/src/main.cpp
@@ -3,12 +3,23 @@
 using namespace std;
 
 extern "C" {
-	void print_hello ();
+	struct CruelRuntime;
+	
+	CruelRuntime * cruel_rt_init ();
+	void cruel_rt_cleanup (CruelRuntime *);
+	void cruel_rt_hello (const CruelRuntime *);
 }
 
 int main () {
 	cout << "Hello from C++!\n";
-	print_hello ();
+	
+	auto rt = cruel_rt_init ();
+	
+	cruel_rt_hello (rt);
+	
+	cruel_rt_cleanup (rt);
+	
+	cout << "Good-bye from C++!\n";
 	
 	return 0;
 }
--- cruel_ruin_rs/src/lib.rs
+++ cruel_ruin_rs/src/lib.rs
@@ -1,4 +1,50 @@
+use std::{
+	time::Duration,
+};
+
+pub struct CruelRuntime {
+	rt: tokio::runtime::Runtime,
+}
+
 #[no_mangle]
-pub extern "C" fn print_hello () {
-	println! ("Hello from Rust!");
+pub extern "C" fn cruel_rt_init () -> Box <CruelRuntime> {
+	let rt = tokio::runtime::Runtime::new ().unwrap ();
+	
+	Box::new (CruelRuntime {
+		rt,
+	})
+}
+
+#[no_mangle]
+pub extern "C" fn cruel_rt_cleanup (rt: *mut CruelRuntime) {
+	if rt.is_null () {
+		return;
+	}
+	
+	let rt = unsafe {
+		Box::from_raw (rt)
+	};
+	
+	std::mem::drop (rt);
+}
+
+#[no_mangle]
+pub extern "C" fn cruel_rt_hello (rt: *const CruelRuntime) {
+	if rt.is_null () {
+		return;
+	}
+	
+	let rt = unsafe {
+		&*rt
+	};
+	
+	rt.rt.block_on (async {
+		use tokio::time::sleep;
+		
+		println! ("Hello 1 from Rust");
+		sleep (Duration::from_secs (1)).await;
+		println! ("Hello 2");
+		sleep (Duration::from_secs (1)).await;
+		println! ("Hello 3");
+	});
 }
