5bb7d6ef971e46afddb78be1add61903cfb47b30
_ <_@_>
2021-12-15 04:56:41 +0000

    :heavy_plus_sign: show how to wrap the trust-dns resolver in a C API for C++ to use

--- cruel_ruin_cpp/src/main.cpp
+++ cruel_ruin_cpp/src/main.cpp
@@ -1,23 +1,87 @@
 #include <iostream>
 
+#include <curl/curl.h>
+
 using namespace std;
 
 extern "C" {
 	struct CruelRuntime;
+	struct CruelString;
+	struct CruelStr {
+		const uint8_t * data;
+		size_t len;
+	};
 	
 	CruelRuntime * cruel_rt_init ();
 	void cruel_rt_cleanup (CruelRuntime *);
-	void cruel_rt_hello (const CruelRuntime *);
+	CruelString * cruel_rt_resolve (const CruelRuntime *, const CruelStr *);
+	
+	uint8_t * cruel_string_ptr (const CruelString *);
+	size_t cruel_string_len (const CruelString *);
+	void cruel_string_free (CruelString *);
+}
+
+size_t write_callback (char *ptr, size_t size, size_t nmemb, void *userdata) {
+	// Just ignore all the data.
+	return size * nmemb;
 }
 
 int main () {
-	cout << "Hello from C++!\n";
-	
+	curl_global_init (CURL_GLOBAL_DEFAULT);
 	auto rt = cruel_rt_init ();
 	
-	cruel_rt_hello (rt);
+	cout << "Hello from C++!\n";
+	
+	auto curl = curl_easy_init ();
+	
+	const string host_str ("example.com");
+	const auto url = "https://" + host_str;
+	cout << "URL: " << url << '\n';
+	curl_easy_setopt (curl, CURLOPT_URL, url.c_str ());
+	curl_easy_setopt (curl, CURLOPT_WRITEFUNCTION, write_callback);
+	
+	curl_slist * slist = nullptr;
+	
+	{
+		// Bunch of dumb FFI stuff needed to make C++ and Rust both pretend
+		// they're actually pieces of C code. There is a cleaner way to do
+		// this, but it requires more preparation time.
+		
+		const CruelStr host_str_c {
+			// C++ thinks strings are arrays of characters, this is cause C++ is
+			// based on C, and C is dumb.
+			(const uint8_t *)host_str.data (),
+			host_str.size (),
+		};
+		auto ip_str_c = cruel_rt_resolve (rt, &host_str_c);
+		const string ip_str ((const char *)cruel_string_ptr (ip_str_c), cruel_string_len (ip_str_c));
+		cruel_string_free (ip_str_c);
+		ip_str_c = nullptr;
+		
+		
+		const auto resolve_str = host_str + ":443:" + ip_str;
+		cout << "Resolve string: " << resolve_str << '\n';
+		slist = curl_slist_append (slist, resolve_str.c_str ());
+		
+		curl_easy_setopt (curl, CURLOPT_RESOLVE, slist);
+	}
+	
+	auto rc = curl_easy_perform (curl);
+	if (rc != CURLE_OK) {
+		cout << "Everything is OK.\n";
+	}
+	else {
+		cerr << "Everything is not OK: " << rc << '\n';
+	}
+	
+	curl_slist_free_all (slist);
+	slist = nullptr;
+	
+	curl_easy_cleanup (curl);
+	curl = nullptr;
 	
 	cruel_rt_cleanup (rt);
+	curl_global_cleanup();
 	
 	cout << "Good-bye from C++!\n";
 	
--- cruel_ruin_rs/src/lib.rs
+++ cruel_ruin_rs/src/lib.rs
@@ -1,16 +1,34 @@
-use std::{
-	time::Duration,
-};
+use trust_dns_resolver::TokioAsyncResolver;
+use trust_dns_resolver::config::*;
 
 pub struct CruelRuntime {
+	resolver: TokioAsyncResolver,
 	rt: tokio::runtime::Runtime,
+}
+
+// There are probably some inefficiences in how this is implemented.
+// It may leak, too.
+pub struct CruelString (String);
+
+#[repr (C)]
+pub struct CruelStr {
+	data: *const u8,
+	len: usize,
 }
 
 #[no_mangle]
 pub extern "C" fn cruel_rt_init () -> Box <CruelRuntime> {
 	let rt = tokio::runtime::Runtime::new ().unwrap ();
 	
+	let resolver = rt.block_on (async {
+		TokioAsyncResolver::tokio (
+			ResolverConfig::default (),
+			ResolverOpts::default ()
+		)
+	}).expect ("failed to connect resolver");
+	
 	Box::new (CruelRuntime {
+		resolver,
 		rt,
 	})
 }
@@ -29,22 +47,93 @@ pub extern "C" fn cruel_rt_cleanup (rt: *mut CruelRuntime) {
 }
 
 #[no_mangle]
-pub extern "C" fn cruel_rt_hello (rt: *const CruelRuntime) {
-	if rt.is_null () {
-		return;
+pub extern "C" fn cruel_rt_resolve (
+	rt: *const CruelRuntime,
+	host: *const CruelStr,
+) -> Box <CruelString> 
+{
+	// I promise regular Rust code is not this ugly.
+	// This is because I'm writing the FFI stuff by hand, which is stupid,
+	// but I know how to do it, and I don't feel like learning about cxxbind
+	// or whatever the new stuff is.
+	
+	if 
+		rt.is_null () ||
+		host.is_null ()
+	{
+		return Box::new (CruelString ("".to_string ()));
 	}
 	
 	let rt = unsafe {
 		&*rt
 	};
 	
-	rt.rt.block_on (async {
-		use tokio::time::sleep;
-		
-		println! ("Hello 1 from Rust");
-		sleep (Duration::from_secs (1)).await;
-		println! ("Hello 2");
-		sleep (Duration::from_secs (1)).await;
-		println! ("Hello 3");
+	let host = unsafe {
+		&*host
+	};
+	
+	if host.data.is_null () {
+		return Box::new (CruelString ("".to_string ()));
+	}
+	
+	let host = unsafe {
+		std::slice::from_raw_parts (host.data, host.len)
+	};
+	
+	// Normally Rust code would have error handling, too.
+	let host = match std::str::from_utf8 (host) {
+		Ok (x) => x,
+		_ => return Box::new (CruelString ("".to_string ())),
+	};
+	
+	let response = rt.rt.block_on (async {
+		rt.resolver.ipv4_lookup (host).await.unwrap ()
 	});
+	
+	let ip_str = response.iter ().next ()
+	.map (|x| format! ("{:?}", x))
+	.unwrap_or_else (|| "".to_string ());
+	
+	println! ("Rust resolved the IP as {}", ip_str);
+	
+	Box::new (CruelString (ip_str))
+}
+
+#[no_mangle]
+pub extern "C" fn cruel_string_ptr (s: *const CruelString) -> *const u8 {
+	if s.is_null () {
+		panic! ("cruel_string_ptr got nullptr");
+	}
+	
+	let s = unsafe {
+		&*s
+	};
+	
+	s.0.as_ptr ()
+}
+
+#[no_mangle]
+pub extern "C" fn cruel_string_len (s: *const CruelString) -> usize {
+	if s.is_null () {
+		panic! ("cruel_string_len got nullptr");
+	}
+	
+	let s = unsafe {
+		&*s
+	};
+	
+	s.0.len ()
+}
+
+#[no_mangle]
+pub extern "C" fn cruel_string_free (s: *mut CruelString) {
+	if s.is_null () {
+		return;
+	}
+	
+	let s = unsafe {
+		Box::from_raw (s)
+	};
+	
+	std::mem::drop (s);
 }
